5708ab9be9209b5737a52613960f21a06f2e1e82
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-03-06 22:04:55 +0800

    new structure

--- Vue3/online-test-system/src/views/HomeView.vue
+++ Vue3/online-test-system/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-   
-  </main>
+  <div class="common-layout">
+    <el-container style="height: 100vh;">
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
---
+++ Vue3/online-test-system/src/views/TestView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="register">
+      <h1>考试页</h1>
+    </div>
+  </template>
+  
+  <style>
+  </style>
+  
