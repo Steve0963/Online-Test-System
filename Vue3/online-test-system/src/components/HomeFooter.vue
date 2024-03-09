d36373009916563b5564092fc1a8df847f6683c3
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-03-10 07:29:59 +0800

    初步完成主页底部

---
+++ Vue3/online-test-system/src/components/HomeFooter.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="left-logo"><img src="../assets/png/foot_left_logo.png" style=""></div>
+        <el-divider class="vertical-divder" direction="vertical" />
+        
+        <div>
+           <el-divider class="horizon-divider"/>
+            
+            <div class="word-div">
+                <div><span>电话：0731-58290011 </span><span style="margin-left: 35px;">湖南省湘潭市雨湖区桃园路湖南科技大学</span></div>
+                    <div style="margin-top: 13px;">Copyright ©2020 湖南科技大学 湘ICP备10001692号-1、湘ICP备10001692号-2工信部备案查询网址 湘公网安备 43030202001075号</div>
+            </div>
+            
+        </div >
+        <div class="right-logo" ><img src="../assets/png/foot_right_logo.png" style=""></div>
+        
+       
+
+</template>
+
+<script>
+
+</script>
+<style scoped>
+.left-logo{
+    width:120px;
+}
+.right-logo{
+    width:256px;
+    height:70px;
+    margin-top: 24px;
+}
+.word-div{
+    color: hsla(0, 0%, 100%, .6);
+    font-size: 13px;
+    
+}
+.vertical-divder{
+margin-left: 50px;
+margin-right: 50px;
+height: 120px;
+}
+    
+.horizon-divider{
+    width: 1470px;
+    margin-right: 50px;
+    
+}
+
+</style>
