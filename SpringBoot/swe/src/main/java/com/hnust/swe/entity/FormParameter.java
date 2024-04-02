b276177a7324633eb7406ab8059ccf7c6da8ceb6
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-02 23:09:57 +0800

    模块化前端表单参数命称

---
+++ SpringBoot/swe/src/main/java/com/hnust/swe/entity/FormParameter.java
@@ -0,0 +1,15 @@
+package com.hnust.swe.entity;
+public class FormParameter {
+
+    public static class Login {
+        public static final String ACCOUNT = "account";
+        public static final String PASSWORD = "password";
+    }
+
+    public static class Registr {
+        public static final String ACCOUNT = "account";
+        public static final String PASSWORD = "password";
+        // 添加其他注册参数...
+    }
+    
+}
