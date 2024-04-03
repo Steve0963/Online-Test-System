5fb4f0c002d4aa2628db99f8e6bdb29ec64ffd74
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-03 21:24:39 +0800

    移除验证码功能

--- Vue3/online-test-system/src/views/LoginView.vue
+++ Vue3/online-test-system/src/views/LoginView.vue
@@ -1,47 +1,34 @@
 <script lang="ts" setup>
+import { useRouter} from 'vue-router';
 import { Ref, reactive, ref } from 'vue'
 import type { FormInstance, FormRules } from 'element-plus'
 import type { TabsPaneContext } from 'element-plus'
-import { User, Lock, MessageBox } from '@element-plus/icons-vue'
-import Authenticator from '../components/vertify/Authenticator.vue'
-
-import { login } from '../requests/api'
-const identifyCode = ref();
-const identifyCodes = ref("1234567890abcdefjhijklinopqrsduvwxyz");
-
-
-// 重置验证码
-const refreshCode = () => {
-    identifyCode.value = "";
-    makeCode(identifyCode, 4);
-};
-
-const makeCode = (o: Ref<any>, l: number) => {
-    for (let i = 0; i < l; i++) {
-        identifyCode.value += identifyCodes.value[randomNum(0, identifyCodes.value.length)];
-    }
-};
-
-
-const randomNum = (min: number, max: number) => {
-    return Math.floor(Math.random() * (max - min) + min);
-};
-
-const regrefreshCode = () => {
-    identifyCode.value = "";
-    makeCode(identifyCode, 4);
-};
-
+import { User, Lock} from '@element-plus/icons-vue'
+import { login,register } from '../requests/api'
+import { useStore } from 'vuex';
 
 const activeName = ref('login')
+const loginFormRef = ref<FormInstance>()
+const RegisterFormRef = ref<FormInstance>()
+const router = useRouter();
+const store = useStore();
+
+const loginForm = reactive({
+    account: '',
+    password: '',
+})
+
+const RegisterForm = reactive({
+    account: '',
+    password: '',
+    crt_pass:'',
+})
 
 const handleClick = (tab: TabsPaneContext, event: Event) => {
     console.log(tab, event)
 }
-const loginFormRef = ref<FormInstance>()
 
 const validateAccount = (rule: any, value: any, callback: any) => {
-    console.log(value)
     if (!value) {
         return callback(new Error('请输入账号'));
     }
@@ -70,39 +57,79 @@ const validatePassword = (rule: any, value: any, callback: any) => {
         }
     }
 }
-const validateCode = (rule: any, value: any, callback: any) => {
-    console.log(identifyCode.value)
-    console.log(value)
-    if (!value) {
-        callback(new Error('请输入验证码'));
-    } else {
-        if (value!==identifyCode.value) {
-            callback(new Error('验证码错误！'));
-        }  else {
-            callback();
-        }
+
+const validateCrt_pass = (rule: any, value: any, callback: any) => {
+    if (RegisterForm.crt_pass!=RegisterForm.password){
+
+        callback(new Error('两次输入不一致'));
     }
-    
+    else{callback();}
 }
 
-const loginForm = reactive({
-    account: '',
-    password: '',
-    code: '',
-})
 
-const rules = reactive<FormRules<typeof loginForm>>({
+
+
+const login_rules = reactive<FormRules<typeof loginForm>>({
     account: [{ validator: validateAccount, trigger: 'blur' }],
     password: [{ validator: validatePassword, trigger: 'change' }],
-    code: [{ validator: validateCode, trigger: 'blur' }],
 })
 
-const submitForm = (formEl: FormInstance | undefined) => {
+const register_rules = reactive<FormRules<typeof RegisterForm>>({
+    account: [{ validator: validateAccount, trigger: 'blur' }],
+    password: [{ validator: validatePassword, trigger: 'change' }],
+    crt_pass:[{ validator: validateCrt_pass, trigger: 'change' }]
+})
+
+const submitLogin = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.validate(async (valid) => {
+        if (valid) {
+            console.log('登录')
+            const result=await (await login(loginForm)).data.data
+            console.log(result)
+
+            if(result != null) {
+                store.dispatch('login', { role: result.role, account: result.account,name:result.name });
+                console.log( store.state)
+          switch(result.role) {
+            case 0:  //管理员
+            
+              console.log("跳转")
+              router.push('/home');
+              
+              break
+            case 1: //教师
+            
+              console.log("跳转")
+              router.push('/home/teacher/');
+              break
+            case 2: //学生
+            
+              
+              console.log("跳转")
+              router.push('/home/student/test');
+              break
+          }
+        }
+
+        if(result == null) { //错误提示
+          
+        }
+            
+        } else {
+            console.log('error submit!')
+            return false
+        }
+    })
+}
+
+const submitRegister = (formEl: FormInstance | undefined) => {
     if (!formEl) return
     formEl.validate((valid) => {
         if (valid) {
-            console.log('submit!')
-           login(loginForm)
+            console.log('注册')
+            register(RegisterForm)
+
         } else {
             console.log('error submit!')
             return false
@@ -128,7 +155,7 @@ const submitForm = (formEl: FormInstance | undefined) => {
                     <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
 
                         <el-tab-pane label="登录" name="login" class="custom-tab">
-                            <el-form ref="loginFormRef" :model="loginForm" status-icon :rules="rules" class="loginForm">
+                            <el-form ref="loginFormRef" :model="loginForm" status-icon :rules="login_rules" class="loginForm">
 
                                 <el-form-item prop="account">
                                     <el-input v-model="loginForm.account" placeholder="请输入账号" :prefix-icon="User" />
@@ -139,52 +166,34 @@ const submitForm = (formEl: FormInstance | undefined) => {
                                         :prefix-icon="Lock" show-password />
                                 </el-form-item>
 
-                                <el-form-item prop="code">
-                                    <el-input v-model="loginForm.code" placeholder="输入验证码" :prefix-icon="MessageBox"
-                                        autocomplete="off">
-                                        <template v-slot:append>
-                                            <Authenticator :identifyCode="identifyCode" @click="refreshCode" />
-                                        </template>
-                                    </el-input>
-                                </el-form-item>
-
                                 <el-form-item>
-                                    <el-button type="primary" @click="submitForm(loginFormRef)">登录</el-button>
+                                    <el-button type="primary" @click="submitLogin(loginFormRef)">登录</el-button>
                                 </el-form-item>
                             </el-form>
                         </el-tab-pane>
 
                         <el-tab-pane label="注册" name="register" class="custom-tab">
-                            <el-form ref="loginFormRef" :model="loginForm" status-icon :rules="rules" label-width="auto"
+                            <el-form ref="RegisterFormRef" :model="RegisterForm" status-icon :rules="register_rules" label-width="auto"
                                 class="demo-loginForm">
 
-                                <el-form-item prop="age">
-                                    <el-input v-model.number="loginForm.password" placeholder="请输入账号"
+                                <el-form-item prop="account">
+                                    <el-input v-model="RegisterForm.account" placeholder="请输入账号"
                                         :prefix-icon="User" />
                                 </el-form-item>
 
-                                <el-form-item prop="pass">
-                                    <el-input v-model="loginForm.password" type="password" placeholder="输入密码"
-                                        autocomplete="off" :prefix-icon="Lock" />
+                                <el-form-item prop="password">
+                                    <el-input v-model="RegisterForm.password" type="password" placeholder="输入密码"
+                                        autocomplete="off" :prefix-icon="Lock" show-password/>
                                 </el-form-item>
 
-                                <el-form-item prop="checkPass">
-                                    <el-input v-model="loginForm.password" type="password" placeholder="再次输入"
-                                        autocomplete="off" :prefix-icon="Lock" />
+                                <el-form-item prop="crt_pass">
+                                    <el-input v-model="RegisterForm.crt_pass" type="password" placeholder="再次输入"
+                                        autocomplete="off" :prefix-icon="Lock" show-password/>
                                 </el-form-item>
 
-                                <el-form-item prop="checkPass">
-                                    <el-input v-model="loginForm.code" placeholder="输入验证码" autocomplete="off"
-                                        :prefix-icon="MessageBox">
-
-                                        <template v-slot:append>
-                                            <Authenticator :identifyCode="identifyCode" @click="regrefreshCode" />
-                                        </template>
-                                    </el-input>
-                                </el-form-item>
 
                                 <el-form-item>
-                                    <el-button type="primary" @click="submitForm(loginFormRef)">注册</el-button>
+                                    <el-button type="primary" @click="submitRegister(RegisterFormRef)">注册</el-button>
                                 </el-form-item>
                             </el-form>
                         </el-tab-pane>
@@ -224,4 +233,4 @@ const submitForm = (formEl: FormInstance | undefined) => {
     font-size: xx-large;
     color: aliceblue;
 }
-</style>../components/vertify/Authenticator.vue
+</style>
