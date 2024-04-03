a06d289da7c13ec5bb25a881191baba0c130126f
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-03 21:24:11 +0800

    增加工具库(获取cookie信息)

---
+++ Vue3/online-test-system/src/components/home/utils/tool.js
@@ -0,0 +1,13 @@
+// utils/cookieUtils.js
+export function getCookie(name) {
+    const cookieString = document.cookie;
+    const cookies = cookieString.split(';');
+    for (let cookie of cookies) {
+      const [cookieName, cookieValue] = cookie.trim().split('=');
+      if (cookieName === name) {
+        return cookieValue;
+      }
+    }
+    return null;
+  }
+  
