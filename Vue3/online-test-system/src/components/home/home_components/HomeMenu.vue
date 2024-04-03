07d5831b4803c68fe2f8a69fa3e9aa8f7206bd89
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-03 21:22:07 +0800

    增加教师端管理端左菜单栏

--- Vue3/online-test-system/src/components/home/home_components/HomeMenu.vue
+++ Vue3/online-test-system/src/components/home/home_components/HomeMenu.vue
@@ -1,53 +1,108 @@
 <template>
 
     <el-menu
+        v-if="role === '2'"
         :router="true"
+        :default-active="currentPage"
+        class="menu"
         @open="handleOpen"
-        @close="handleClose"
-        
+        background-color=#d9e0e9
       >
       
-      <el-menu-item index="/home/test">
-        <el-icon><icon-menu /></el-icon>
+      <el-menu-item index="/home/student/test" >
+        <el-icon><Clock /></el-icon>
         <template #title>考试信息</template>
       </el-menu-item>
-      <el-menu-item index="/home/class">
-        <el-icon><document /></el-icon>
+
+      <el-menu-item index="/home/student/class" >
+        <el-icon ><IconMenu/></el-icon>
         <template #title>我的班级</template>
       </el-menu-item>
 
-      <el-menu-item index="/home/user">
-        <el-icon><setting /></el-icon>
-        <template #title>个人信息</template>
-      </el-menu-item>
-
-      <el-menu-item index="/home/score">
-        <el-icon><setting /></el-icon>
+      <el-menu-item index="/home/student/score" >
+        <el-icon><document /></el-icon>
         <template #title>我的成绩</template>
       </el-menu-item>
 
+      <el-menu-item index="/home/student/message">
+        <el-icon><Message /></el-icon>
+        <template #title>系统信息</template>
+      </el-menu-item>
 
+      <el-menu-item index="/home/student/user">
+        <el-icon><User /></el-icon>
+        <template #title>个人信息</template>
+      </el-menu-item>
 
-       <!-- <el-sub-menu index="/home/score">
+    </el-menu>
+
+    <el-menu
+    v-if="role ==='1' "
+    :router="true"
+    :default-active="currentPage"
+    class="menu"
+    @open="handleOpen"
+    background-color=#d9e0e9
+  >
+      <el-menu-item index="/home/teacher">
+        <el-icon><document /></el-icon>
+        <template #title>主页</template>
+      </el-menu-item>
+
+      <el-menu-item index="/home/teacher/class">
+        <el-icon><document /></el-icon>
+        <template #title>班级管理</template>
+      </el-menu-item>
+
+      
+      <el-menu-item index="/home/teacher/exam">
+        <el-icon><Message /></el-icon>
+        <template #title>考试管理</template>
+      </el-menu-item>
+
+  
+      <el-sub-menu index="paper">
         <template #title>
-          <el-icon><location /></el-icon>
-          <span>我的成绩</span>
-        </template>
-        <el-menu-item-group>
-          <template #title><span>Group One</span></template>
-          <el-menu-item index="1-1">item one</el-menu-item>
-          <el-menu-item index="1-2">item two</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-          <el-menu-item index="1-3">item three</el-menu-item>
-        </el-menu-item-group>
-        <el-sub-menu index="1-4">
-          <template #title><span>item four</span></template>
-          <el-menu-item index="1-4-1">item one</el-menu-item>
-        </el-sub-menu>
-      </el-sub-menu>  -->
+          <el-icon><document /></el-icon>试卷管理</template>
+        <el-menu-item index="/home/teacher/paperlist">试卷列表</el-menu-item>
+        <el-menu-item index="/home/teacher/papercreate">创建试卷</el-menu-item>
+      </el-sub-menu>
 
-      </el-menu>
+
+      <el-sub-menu index='problem'>
+        <template #title>
+          <el-icon><document /></el-icon>题库管理</template>
+        <el-menu-item index="/home/teacher/problemlist">题目列表</el-menu-item>
+        <el-menu-item index="/home/teacher/problemcreate">创建题目</el-menu-item>
+      </el-sub-menu>
+
+    </el-menu>
+
+   <el-menu
+    v-if="role === '0'"
+    :router="true"
+    class="menu"
+    :default-active="currentPage"
+    @open="handleOpen"
+    background-color=#d9e0e9
+  >
+      <el-menu-item index="/home/score" >
+        <el-icon><document /></el-icon>
+        <template #title>试卷管理</template>
+      </el-menu-item>
+
+      <el-menu-item index="/home/message">
+        <el-icon><Message /></el-icon>
+        <template #title>安排考试</template>
+      </el-menu-item>
+
+      <el-menu-item index="/home/user">
+        <el-icon><User /></el-icon>
+        <template #title>题库管理</template>
+      </el-menu-item>
+
+    </el-menu>
+    
 
 
       
@@ -58,18 +113,33 @@
 import {
   Document,
   Menu as IconMenu,
-  Location,
-  Setting,
+  User,
+  Clock,
+  Message,
 } from '@element-plus/icons-vue'
+import { useStore } from 'vuex';
+import { getCookie } from '../utils/tool'
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const store = useStore();
+const role = getCookie('role');
+const isLoggedIn =getCookie('isLoggedIn');
+var currentPage
+
 const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
+  currentPage=route.path
+  console.log(currentPage)
 }
 const handleClose = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
+
 </script>
 
   
   <style>
+.menu{
+  height:77vh;
+}
   </style>
   
