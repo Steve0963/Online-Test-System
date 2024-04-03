8f3c96a8bfe2c7138955cfc56953445d0ececb59
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-03 21:19:25 +0800

    增加store和cookie对登录信息存储

--- Vue3/online-test-system/src/main.js
+++ Vue3/online-test-system/src/main.js
@@ -1,8 +1,7 @@
-
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store';
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -11,5 +10,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
 app.use(router)
+app.use(store)
 app.use(ElementPlus)
 app.mount('#app')
---
+++ Vue3/online-test-system/src/store/cookiePlugin.js
@@ -0,0 +1,31 @@
+
+export const cookiePlugin= store => {
+    // 监听 mutation 的变化
+    store.subscribe((mutation, state) => {
+      // 在 mutation 中存储数据到 Cookie 中
+      switch (mutation.type) {
+        case 'login':
+          // 设置登录状态到 Cookie,过期时间为 7 天
+          setCookie('isLoggedIn', state.isLoggedIn, 7); 
+          setCookie('account', state.account, 7); 
+          setCookie('name', state.name, 7); 
+          setCookie('role', state.role, 7);
+          break;
+        case 'logout':
+            setCookie('isLoggedIn', state.isLoggedIn, 7); 
+            setCookie('account', state.account, 7); 
+            setCookie('name', state.name, 7); 
+            setCookie('role', state.role, 7);// 设置用户账号在 Cookie 中的过期时间为 7 天
+            break;
+        
+      }
+    });
+  };
+  
+  // 设置 Cookie 的函数
+  function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
