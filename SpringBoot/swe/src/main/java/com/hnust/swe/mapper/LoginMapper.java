e38985c6f9233fca6433d85643fcc760f83539ed
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-03 21:26:48 +0800

    修改登录逻辑sql语句

--- SpringBoot/swe/src/main/java/com/hnust/swe/mapper/LoginMapper.java
+++ SpringBoot/swe/src/main/java/com/hnust/swe/mapper/LoginMapper.java
@@ -1,8 +1,8 @@
 package com.hnust.swe.mapper;
+
 import com.hnust.swe.entity.LoginResult;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
 
 @Mapper
 public interface LoginMapper {
@@ -10,9 +10,9 @@ public interface LoginMapper {
     @Select("select role from admin_account where account = #{account} and password = #{password}")
     public LoginResult adminLogin(String account, String password);
 
-    @Select("select role from teacher_account where account = #{account} and password = #{password}")
+    @Select("SELECT teacher_account.account,teacher_account.role, teacher_info.name from teacher_account INNER JOIN teacher_info ON teacher_account.account = teacher_info.account WHERE teacher_account.account = #{account} and teacher_account.password = #{password}")
     public LoginResult teacherLogin(String account, String password);
 
-    @Select("select role from stu_account where account = #{account} and password = #{password}")
-    public LoginResult studentLogin(String account,String password);
+    @Select("SELECT stu_account.account, stu_account.role, stu_info.name from stu_account INNER JOIN stu_info ON stu_account.account = stu_info.account WHERE stu_account.account = #{account} and stu_account.password = #{password}")
+    public LoginResult studentLogin(String account, String password);
 }
