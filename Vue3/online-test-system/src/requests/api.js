c92310885e0438d9fd21ac99288e9f667c142fc8
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-05 19:37:25 +0800

    增加创建班级和加载班级列表的api

--- Vue3/online-test-system/src/requests/api.js
+++ Vue3/online-test-system/src/requests/api.js
@@ -3,9 +3,11 @@ const baseUrl = 'http://localhost:8080';
 
 export const login = (formData) => {
   const url = `${baseUrl}/login`;
-
+console.log(formData)
   return axios.post(url, formData)
-    .then(response => response.data)
+    .then(response => {
+      return response
+    })
     .catch(error => {
       console.error('Error submitting form:', error);
       throw error;
@@ -17,6 +19,33 @@ export const register = (formData) => {
   
     return axios.post(url, formData)
       .then(response => response.data)
+      .catch(error => {
+        console.error('Error submitting form:', error);
+        throw error;
+      });
+  };
+
+
+  export const creatClass = (formData) => {
+    const url = `${baseUrl}/creatClass`;
+  console.log(formData)
+    return axios.post(url, formData)
+      .then(response => {
+        return response
+      })
+      .catch(error => {
+        console.error('Error submitting form:', error);
+        throw error;
+      });
+  };
+
+  export const loadClass = (formData) => {
+    const url = `${baseUrl}/loadClass`;
+  console.log(formData)
+    return axios.post(url, formData)
+      .then(response => {
+        return response
+      })
       .catch(error => {
         console.error('Error submitting form:', error);
         throw error;
