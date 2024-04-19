e71dda37cd0b3c4d3da8499178a283efdf5c43ba
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-20 01:05:18 +0800

    更正参数名

--- SpringBoot/swe/src/main/java/com/hnust/swe/entity/ClassListResult.java
+++ SpringBoot/swe/src/main/java/com/hnust/swe/entity/ClassListResult.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 public class ClassListResult {
 
     private String class_id;
-    private String class_name;
+    private String name;
     private String class_token;
     private Date creat_time;
     private int class_num;
