206216ccc70069e322dd1feaa29db0c42b2fe0d1
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-20 01:09:14 +0800

    完成登录相关sql语句

--- SpringBoot/swe/src/main/java/com/hnust/swe/mapper/LoginMapper.java
+++ SpringBoot/swe/src/main/java/com/hnust/swe/mapper/LoginMapper.java
@@ -10,7 +10,7 @@ public interface LoginMapper {
     @Select("select role from admin_account where account = #{account} and password = #{password}")
     public LoginResult adminLogin(String account, String password);
 
-    @Select("SELECT teacher_account.account,teacher_account.role, teacher_info.name from teacher_account INNER JOIN teacher_info ON teacher_account.account = teacher_info.account WHERE teacher_account.account = #{account} and teacher_account.password = #{password}")
+    @Select("SELECT teacher_account.account,teacher_account.role, teacher_info.name ,teacher_info.teacher_id from teacher_account INNER JOIN teacher_info ON teacher_account.account = teacher_info.account WHERE teacher_account.account = #{account} and teacher_account.password = #{password}")
     public LoginResult teacherLogin(String account, String password);
 
     @Select("SELECT stu_account.account, stu_account.role, stu_info.name from stu_account INNER JOIN stu_info ON stu_account.account = stu_info.account WHERE stu_account.account = #{account} and stu_account.password = #{password}")
