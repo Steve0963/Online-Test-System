cdea110c859207832ced97803a99a9fa42ca8b3c
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-20 01:10:23 +0800

    增加request模块

--- Vue3/online-test-system/src/main.js
+++ Vue3/online-test-system/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store';
+import * as requests from './requests/api'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -12,4 +13,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
+app.use(requests)
 app.mount('#app')
