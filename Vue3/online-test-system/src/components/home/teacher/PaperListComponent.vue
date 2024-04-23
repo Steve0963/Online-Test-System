192576509cbdfbd53200714c4830820457093504
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-24 03:17:25 +0800

    创建页面模板

--- Vue3/online-test-system/src/components/home/teacher/PaperListComponent.vue
+++ Vue3/online-test-system/src/components/home/teacher/PaperListComponent.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
-import { getCookie } from '../utils/tool'
+import { getCookie,getCurrentPage } from '../utils/tool'
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const isLoggedIn = getCookie('isLoggedIn') === 'true';
+const role = getCookie('role');
+
+
+if (!isLoggedIn) {
+  router.push('/');
+} else if (role !== '1') {
+  
+  router.push('/');
+}
+
 
 </script>
 
