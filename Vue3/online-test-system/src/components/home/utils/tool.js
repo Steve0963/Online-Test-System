5d1982e6924b6fd8172f26462fca24961b59a155
QuoVadis <11589357+quovadis8@user.noreply.gitee.com>
2024-04-24 03:16:00 +0800

    将共用函数合并到tools里面

--- Vue3/online-test-system/src/components/home/utils/tool.js
+++ Vue3/online-test-system/src/components/home/utils/tool.js
@@ -1,4 +1,5 @@
 // utils/cookieUtils.js
+var currentPage=null
 export function getCookie(name) {
     const cookieString = document.cookie;
     const cookies = cookieString.split(';');
@@ -10,4 +11,33 @@ export function getCookie(name) {
     }
     return null;
   }
-  
+
+
+export function setCurrentPage(path){
+  localStorage.setItem('page',path)
+}
+export function getCurrentPage(){
+  return localStorage.getItem('page')
+  }
+
+
+ export function getExamType (type){
+    if(type==1) return "线上"
+    return "线下"
+  }
+
+  export function getTagType (score){
+    if (score >= 90) return 'success'
+    else if (score >= 80) return 'primary'
+    else if (score >= 70) return 'info'
+    else if (score >= 60) return 'warning'
+    else return 'danger'
+  }
+
+  export function getTag (score){
+    if (score>=90) return '优'
+    else if(score>=80) return '良'
+    else if(score>=70) return '中'
+    else if(score>=60) return '差'
+    else return '不及格'
+  }
